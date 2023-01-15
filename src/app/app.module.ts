9a87de5b4de6c15525d849986e105faf4cc6eef3
unknown <tompsherman@gmail.com>
2023-01-15 06:46:24 -0500

    created navbar and list components, populated with fake data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ListComponent } from './list/list.component';
+import { NavComponent } from './navigation/nav.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ListComponent, NavComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'list',
+  templateUrl: './list.component.html',
+  // styleUrls:
+})
+export class ListComponent {
+  // groceryItems:[] = []
+
+  groceryItems = [
+    { amount: 5, item: 'apples', completed: false },
+    { amount: 1, item: 'lb. fish', completed: false },
+    { amount: 3, item: 'golden marmosets', completed: false },
+  ];
+
+  todoItems = [
+    {
+      task: 'create list component',
+      description: 'displays list ',
+      completed: true,
+    },
+    {
+      task: 'add items to list',
+      description: 'add new items',
+      completed: false,
+    },
+    {
+      task: 'update items on list',
+      description: 'edit items',
+      completed: false,
+    },
+    { task: 'delete items', description: 'delete items', completed: false },
+    {
+      task: 'filter/sort items',
+      description: 'sort list by category',
+      completed: false,
+    },
+    { task: 'search items', description: 'filter by input', completed: false },
+    {
+      task: 'set up routing',
+      description: 'dynamic routing',
+      completed: false,
+    },
+    {
+      task: 'CRUD operations',
+      description: 'GET / POST / PUT / DEL',
+      completed: false,
+    },
+  ];
+}
